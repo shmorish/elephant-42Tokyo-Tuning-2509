9f1fa80b1cac413e606f93fd78b8f37d96a5ece0
shmorish <110565242+shmorish@users.noreply.github.com>
2025-09-27 14:11:50 +0900

    fix: alp

--- webapp/nginx/alp.sh
+++ webapp/nginx/alp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cat /var/log/nginx/access.log | \
-    perl -pE 's{/(assets|images)/.*"}{/$1/***"}' | \
+    perl -pE 's{/(assets|images|_next)/[^"]*"}{/$1/***"}' | \
     alp json -o count,method,uri,min,avg,max,sum,2xx,3xx,4xx,5xx -r
